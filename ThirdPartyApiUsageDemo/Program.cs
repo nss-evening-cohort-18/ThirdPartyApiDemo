76fbeefaf945289e85b509d9b054f73b65adaadc
Brian Neal <brianbneal@gmail.com>
2022-11-14 19:43:33 -0600

    made comments more helpful

--- ThirdPartyApiUsageDemo/Program.cs
+++ ThirdPartyApiUsageDemo/Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddTransient<ISwapiClientRepository, SwapiClientRepository>();
 builder.Services.AddTransient<IUserSqlRepository, UserSqlRepository>();
+
+//HttpClient is the class used to make calls to other web APIs.
+//You can use HttpClient with dependency injection by adding it like this.
 builder.Services.AddHttpClient("Swapi", httpClient =>
 {
     httpClient.BaseAddress = new Uri("https://swapi.dev/api/");
@@ -18,6 +21,7 @@ builder.Services.AddHttpClient("Swapi", httpClient =>
     httpClient.DefaultRequestHeaders.Add(
         HeaderNames.Accept, "application/json");
     //include any headers required by the api you're contacting
+    //if you're including API keys, make sure to put them in your secrets.json and grab them out of there.
 });
 
 var app = builder.Build();
--- ThirdPartyApiUsageDemo/Repositories/SwapiClientRepository.cs
+++ ThirdPartyApiUsageDemo/Repositories/SwapiClientRepository.cs
@@ -3,21 +3,26 @@ using ThirdPartyApiUsageDemo.Repositories;
 
 namespace ThirdPartyApiUsageDemo.Clients;
 
+//Note that this is still a repository and its job is still to contact a data source,
+//but I've called it a Client Repository as opposed to SQL Repository.
 public class SwapiClientRepository : ISwapiClientRepository
 {
+    //this will come from dependency injection in the constructor
     private readonly HttpClient _swapiClient;
 
     public SwapiClientRepository(IHttpClientFactory clientFactory)
     {
-        //this IHttpClientFactory is from Services in Program.cs, just like any other dependency injection
-        //the name "Swapi" comes from the setup of this in Program.cs, go check it out.  You can set up
-        //multiple named clients and call the correct name wherever used.
+        //This IHttpClientFactory is from Services in Program.cs, just like any other dependency injection.
+        //The name "Swapi" comes from the setup of this in Program.cs, go check it out.  You can set up
+        //multiple named clients and call the correct name wherever used.  The IHttpClientFactory is used
+        //to procure an instance of our HttpClient.
         _swapiClient = clientFactory.CreateClient("Swapi");
     }
 
     //This GetPeople method is a bit more complex than you would normally have for a GetAll
     //because of the way this particular API returns only 10 results at a time, forcing us
-    //to fetch multiple pages of results.
+    //to fetch multiple pages of results.  If all of your data is returned in one call then you won't have to 
+    //check for result.Next the way I do here.  You can just ReadFromJsonAsync and be done much more quickly.
     public async Task<List<SwapiPerson>?> GetPeople(int page = 1, List<SwapiPerson>? people = null)
     {
         //notice we only have to pass in the end of the URL since the BaseAddress property was set in Program.cs
@@ -41,6 +46,7 @@ public class SwapiClientRepository : ISwapiClientRepository
         return people;
     }
 
+    //This is a much more straightforward example.
     public async Task<SwapiPerson?> GetPerson(int id)
     {
         SwapiPerson? planet = null;
